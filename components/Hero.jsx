0628e6258144a3477a4e6af959131526950954f5
kirti2818 <kirti8527260810@gmail.com>
2023-06-27 16:07:16 +0530

    changes

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,17 +1,46 @@
-import React from 'react'
+"use client"
+import React, { useState } from "react";
+import { AiOutlineMenu ,AiOutlineCloseCircle} from "react-icons/ai";
+
 
 const Hero = () => {
+  const [show,setShow] = useState(false)
   return (
-    <div className="flex justify-between items-center  border border-red-800 text-white h-24 w-[1240px] mx-auto">
-        <div className='w-full text-3xl font-bold text-[#00df9a]'>REACT.</div>
-        <ul className='flex w-full justify-between ' >
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact Us</li>
-            <li>Resources</li>
+   <>
+   {!show ?  <div className="flex justify-between items-center text-white h-24 border border-red-500 px-[10px]">
+      <div className="text-3xl font-bold text-[#00df9a] border bg-yellow-300">
+        REACT.
+      </div>
+      <div className="w-[50%] text-xl flex justify-between items-center">
+        <ul className="w-[70%] flex justify-between">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Resources</li>
         </ul>
-    </div>
-  )
-}
+        <div onClick = {()=>setShow(!show)}>
+          <AiOutlineMenu className="text-3xl" />
+        </div>
+      </div>
+    </div> :  <div  className="flex flex-col ease-in-out duration-300 justify-evenly text-white h-[200px] w-[400px] border border-red-500 px-[10px]">
+     <div className="flex justify-between">
+     <div className=" text-3xl font-bold text-[#00df9a] border bg-yellow-300">
+        REACT.
+      </div>
+      <div onClick = {()=>setShow(!show)}><AiOutlineCloseCircle className="text-3xl" /></div>
+     </div>
+     
+        <ul className="w-[70%] flex flex-col ">
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Resources</li>
+        </ul>
+       
+     
+    </div>}
+   </>
+  );
+};
 
-export default Hero
+export default Hero;
